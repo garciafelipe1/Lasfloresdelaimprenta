e96db856d22fad3ab7faf265a255ee47755112db
Felipe <feli2003garcia@gmail.com>
2025-10-11 12:30:08 -0300

    corregidos token, vercel pull, falta apps/www

--- apps/www/src/app/(app)/[locale]/[countryCode]/(public)/checkout/(multi-step-form)/payment/page.tsx
+++ apps/www/src/app/(app)/[locale]/[countryCode]/(public)/checkout/(multi-step-form)/payment/page.tsx
@@ -22,7 +22,7 @@ export default async function CheckoutPaymentStepPage() {
       <Card>
         <CardHeader>
           <CardTitle>Opciones de pago</CardTitle>
-          Console.log(paymentMethods);
+          
         </CardHeader>
         <CardContent>
           <PaymentForms
