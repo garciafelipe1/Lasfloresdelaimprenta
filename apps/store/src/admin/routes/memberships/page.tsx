d1c2cb0eb95c95440207dda7799d28dcd64421ea
Felipe <feli2003garcia@gmail.com>
2025-10-28 22:29:30 -0300

    yml mejorado: NEXT_PUBLIC_MEDUSA_BACKEND_URL 10

--- apps/store/src/admin/routes/memberships/page.tsx
+++ apps/store/src/admin/routes/memberships/page.tsx
@@ -9,6 +9,7 @@ const CustomPage = () => {
       <div className="flex items-center justify-between px-6 py-4">
         <Heading level="h2">Miembros</Heading>
         console.log("Hola miembros");
+        console.log("Gestiona los miembros aquí");
       </div>
       <Members />
     </Container>
