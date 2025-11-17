df6922f48571029255504f68d20c4f8d16a54ab0
Felipe <feli2003garcia@gmail.com>
2025-11-17 16:17:18 -0300

    modificando el package json de store 3

--- apps/store/src/admin/routes/memberships/page.tsx
+++ apps/store/src/admin/routes/memberships/page.tsx
@@ -8,7 +8,6 @@ const CustomPage = () => {
     <Container className="divide-y p-0">
       <div className="flex items-center justify-between px-6 py-4">
         <Heading level="h2">Miembros</Heading>
-        console.log("Admin Memberships Page");
       </div>
       <Members />
     </Container>
