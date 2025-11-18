93a91b296c3f732401973a43b4f3f10ce46541e3
Felipe <feli2003garcia@gmail.com>
2025-11-18 01:17:56 -0300

    docker file 10

--- apps/store/src/modules/resend/emails/messages/en/payment-captured.ts
+++
@@ -1,8 +0,0 @@
-const en = {
-  "paymentCaptured.heading": "Payment Captured",
-  "paymentCaptured.message":
-    "Your payment for order #{orderId} has been successfully captured. We're now preparing your order for shipment.",
-  "paymentCaptured.shipment": "We’ll notify you when we ship your order.",
-} as const;
-
-export default en;
--- apps/store/src/modules/resend/emails/messages/es/order-footer.ts
+++
@@ -1,8 +0,0 @@
-const es = {
-  "orderFooter.question":
-    "Si tiene alguna pregunta, responda a este correo electrónico o comuníquese con nuestro equipo de soporte a support@medusajs.com",
-  "orderFooter.token": "Token de pedido: {orderId}",
-  "orderFooter.copyright": "Reservados todos los derechos.",
-};
-
-export default es;
