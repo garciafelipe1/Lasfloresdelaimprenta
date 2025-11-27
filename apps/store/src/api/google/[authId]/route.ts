8bb1ae829c5b70f972917bca553f66343bdaa9de
Felipe <feli2003garcia@gmail.com>
2025-11-27 16:21:11 -0300

    verificando auth sin console.log

--- apps/store/src/api/google/[authId]/route.ts
+++ apps/store/src/api/google/[authId]/route.ts
@@ -6,11 +6,9 @@ export async function GET(
   res: MedusaResponse
 ): Promise<void> {
   const authId = req.params.authId;
-  console.log("El auth ID es: "+authId)
   const authModuleService = req.scope.resolve(Modules.AUTH);
   const identity = (await authModuleService.listProviderIdentities()).filter(
     (iden) => iden.auth_identity_id === authId
   );
-  console.log("La identity es: "+identity)
   res.json(identity.length > 0 ? identity[0].user_metadata : null);
 }
